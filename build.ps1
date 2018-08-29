fd85cee5f295b6c0a6fa2d0e0e5171f22f604769
Aaron Stannard <aaron@petabridge.com>
2018-08-29 13:31:16 -0500

    Revert "updated build scripts to use correct dotnet-install destintation"
    This reverts commit 6d73addefd322ab0f3907fa0b2127acab75848ad.

--- build.ps1
+++ build.ps1
@@ -30,10 +30,11 @@ Param(
 )
 
 $FakeVersion = "4.63.0"
-$DotNetChannel = "LTS";
+$NBenchVersion = "1.0.1"
+$DotNetChannel = "preview";
 $DotNetVersion = "2.0.0";
-$DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/v2.0.0/scripts/obtain/dotnet-install.ps1";
-$NugetVersion = "4.1.0";
+$DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1";
+$NugetVersion = "4.3.0";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/v$NugetVersion/nuget.exe"
 $ProtobufVersion = "3.2.0"
 $DocfxVersion = "2.21.1"
