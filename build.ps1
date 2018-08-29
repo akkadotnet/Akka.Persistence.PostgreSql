2bb83020e5269bc23ce290da0ab0ed7f5b935e86
Dan Warner <dan@ctrlaltdan.com>
2018-08-29 10:32:07 +0100

    Upgrading build tools

--- build.ps1
+++ build.ps1
@@ -29,12 +29,12 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$FakeVersion = "4.61.2"
+$FakeVersion = "4.63.0"
 $NBenchVersion = "1.0.1"
 $DotNetChannel = "preview";
-$DotNetVersion = "1.0.4";
+$DotNetVersion = "2.0.0";
 $DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1";
-$NugetVersion = "4.1.0";
+$NugetVersion = "4.3.0";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/v$NugetVersion/nuget.exe"
 $ProtobufVersion = "3.2.0"
 $DocfxVersion = "2.21.1"
