c27961e8e06be5e88354e751adb2d04a11e16001
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-02-03 01:03:29 +0700

    Update spec, use predictable randomized chaotic seeding

--- src/Akka.Persistence.PostgreSql.Tests/PostgreSqlJournalRecoverySpec.cs
+++ src/Akka.Persistence.PostgreSql.Tests/PostgreSqlJournalRecoverySpec.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Configuration;
 using Akka.TestKit;
+using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -13,6 +16,16 @@ namespace Akka.Persistence.PostgreSql.Tests
     {
         private const int TotalActors = 50;
         private const int TotalPersistedMessages = 100;
+        private static readonly int Seed;
+        private static readonly Random Rnd;
+
+        static PostgreSqlJournalRecoverySpec()
+        {
+            // Generate seed from DateTime
+            var now = DateTime.Now;
+            Seed = now.Year + now.Month + now.Day + now.Hour + now.Minute + now.Second + now.Millisecond;
+            Rnd = new Random(Seed);
+        }
         
         private static Config Initialize(PostgresFixture fixture) 
         {
@@ -47,56 +60,77 @@ namespace Akka.Persistence.PostgreSql.Tests
         [Fact]
         public async Task Recovery_should_emit_messages_in_the_right_order()
         {
-            var probes = new TestProbe[TotalActors];
+            var probe = CreateTestProbe();
+            //var probes = new TestProbe[TotalActors];
             var actors = new IActorRef[TotalActors];
             var states = new int[TotalActors];
 
             // Create persistent actors and populate them with data 
             for (var i = 0; i < TotalActors; i++)
             {
-                probes[i] = CreateTestProbe();
-                actors[i] = Sys.ActorOf(Props.Create(() => new PersistentActor($"test_id_{i}", probes[i])));
-                
-                probes[i].ExpectMsg("Recovered. State: 0");
-                
-                foreach (var data in Enumerable.Range(1, TotalPersistedMessages))
-                {
-                    states[i] += await actors[i].Ask<int>(data);
-                }
+                actors[i] = Sys.ActorOf(Props.Create(() => new PersistentActor(i, probe)));
+                var m = probe.ExpectMsg<(int, string)>();
+                var (idx, msg) = m;
+                idx.Should().Be(i);
+                msg.Should().Contain("Recovered. State: 0");
+            }
+            
+            // Chaotically populate actors with data
+            var lastData = new int[TotalActors];
+            var actorIndices = new List<int>();
+            for (var i = 0; i < TotalActors; i++)
+            {
+                actorIndices.Add(i);
+            }
+
+            while (actorIndices.Count > 0)
+            {
+                var i = actorIndices[Rnd.Next(0, actorIndices.Count)];
+                states[i] += await actors[i].Ask<int>(++lastData[i]);
+                if (lastData[i] == TotalPersistedMessages)
+                    actorIndices.Remove(i);
             }
 
             // Kill all actors
             for (var i = 0; i < TotalActors; i++)
             {
-                probes[i].Watch(actors[i]);
+                probe.Watch(actors[i]);
                 actors[i].Tell(PoisonPill.Instance);
-                probes[i].ExpectTerminated(actors[i]);
-                probes[i].Unwatch(actors[i]);
+                probe.ExpectTerminated(actors[i]);
+                probe.Unwatch(actors[i]);
             }
-            
+
             // Restore all actors
+            probe = CreateTestProbe();
             for (var i = 0; i < TotalActors; i++)
             {
-                actors[i] = Sys.ActorOf(Props.Create(() => new PersistentActor($"test_id_{i}", probes[i])));
+                actors[i] = Sys.ActorOf(Props.Create(() => new PersistentActor(i, probe)));
             }
             
             // All actors should restore fine
             for (var i = 0; i < TotalActors; i++)
             {
-                probes[i].ExpectMsg($"Recovered. State: {states[i]}");
+                var m = probe.ExpectMsg<(int, string)>();
+                var (index, msg) = m;
+                msg.Should().Contain($"Recovered. State: {states[index]}");
+                Output.WriteLine($"{index}. {msg}");
             }
+            
+            // No message should arrive after
+            probe.ExpectNoMsg(TimeSpan.FromSeconds(2));
         }
         
         internal class PersistentActor : ReceivePersistentActor
         {
             private bool _recoveryCompleted;
             private readonly IActorRef _probe;
+            private readonly int _index;
             private int _state;
-            public override string PersistenceId { get; }
+            public override string PersistenceId => $"test_id_{_index}";
 
-            public PersistentActor(string persistenceId, IActorRef probe)
+            public PersistentActor(int index, IActorRef probe)
             {
-                PersistenceId = persistenceId;
+                _index = index;
                 _probe = probe;
                 
                 Command<int>(l =>
@@ -113,7 +147,7 @@ namespace Akka.Persistence.PostgreSql.Tests
                 {
                     if(_recoveryCompleted)
                     {
-                        _probe.Tell($"Persisted {p.Data} arrived after recovery was completed.");
+                        _probe.Tell($"{PersistenceId}: Persisted {p.Data} arrived after recovery was completed. Random seed: {Seed}");
                         return;
                     }
                     
@@ -127,10 +161,10 @@ namespace Akka.Persistence.PostgreSql.Tests
                 {
                     case RecoveryCompleted _:
                         _recoveryCompleted = true;
-                        _probe.Tell($"Recovered. State: {_state}");
+                        _probe.Tell((_index, $"Recovered. State: {_state}"));
                         return;
                     case Persisted p:
-                        _probe.Tell($"Persisted {p.Data} was unhandled");
+                        _probe.Tell($"{PersistenceId}: Persisted {p.Data} was unhandled. Random seed: {Seed}");
                         break;
                 }
                 base.Unhandled(message);
